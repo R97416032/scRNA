52acf4be1df9288a89c49cc2f4cd9ca3d6b0844c
R <97416032@qq.com>
2023-06-24 14:23:42 +0800

    将GSE81812的FPKM转换为波长

---
+++ scrna/GSE81812/wangkun/toWaveLength.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def toWavalen(path):
+    data=pd.read_csv(path)
+    sample=data.columns[2:]
+    geneid=data["gene_id"].values
+    df=pd.DataFrame(columns=["gene_id",*sample])
+    df["gene_id"]=geneid
+    for s in sample:
+        min_expression=min(data[s].values)
+        max_expression=max(data[s].values)
+        res=[]
+        for v in data[s].values:
+            res.append("%.2f"%((v-min_expression)*(780-380)/(max_expression-min_expression)+380))
+        df[s]=res
+    df.to_csv("wavelength.csv",index=None)
+toWavalen("scFPKM.csv")
