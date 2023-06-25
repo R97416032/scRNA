e5f3a04a8d943577fd3494942a0d9e4f8faeba85
R <97416032@qq.com>
2023-06-25 20:31:49 +0800

    优化了直方图显示数据的代码

--- scrna/GSE81812/top500/statistic4Top500.py
+++ scrna/GSE81812/top500/statistic4Top500.py
@@ -3,6 +3,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+from utils.showvalue import show_values
+
 
 def getTop500(path,key="down"):
     data=pd.read_csv(path)
@@ -25,26 +27,7 @@ def getTop500(path,key="down"):
     return df["gene_type"].values,df["res"].values,df["avg"].values
 
 
-def show_values(axs, orient="v", space=.01):
-    def _single(ax):
-        if orient == "v":
-            for p in ax.patches:
-                _x = p.get_x() + p.get_width() / 2
-                _y = p.get_y() + p.get_height() + (p.get_height()*0.01)
-                value = '{:.0f}'.format(p.get_height())
-                ax.text(_x, _y, value, ha="center")
-        elif orient == "h":
-            for p in ax.patches:
-                _x = p.get_x() + p.get_width() + float(space)
-                _y = p.get_y() + p.get_height() - (p.get_height()*0.5)
-                value = '{:.0f}'.format(p.get_width())
-                ax.text(_x, _y, value, ha="left")
 
-    if isinstance(axs, np.ndarray):
-        for idx, ax in np.ndenumerate(axs):
-            _single(ax)
-    else:
-        _single(axs)
 
 path="../GSE81812_Normalized_counts_eachgene_result.csv"
 #绘制图像最少的前500
